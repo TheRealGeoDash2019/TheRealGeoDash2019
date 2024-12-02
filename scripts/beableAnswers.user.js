ef7bf2efdc000f77419cf4bb8e3a2133736c275a
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-12-02 11:16:23 -0500

    Update beableAnswers.user.js

--- scripts/beableAnswers.user.js
+++ scripts/beableAnswers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Beable Answers
 // @namespace    http://tampermonkey.net/
-// @version      0.0.12
+// @version      0.0.13
 // @description  Get free legit answers on Beable using Ancient Chinese Technique
 // @author       TheRealGeoDash
 // @match        *://*.beable.com/*
