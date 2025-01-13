d5fd4fd9c53101f414b2bdb39106e574c90aed13
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2025-01-13 14:07:05 -0500

    Update beableAnswers.user.js

--- scripts/beableAnswers.user.js
+++ scripts/beableAnswers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Beable Answers
 // @namespace    http://tampermonkey.net/
-// @version      0.0.20
+// @version      0.0.22
 // @description  Get free legit answers on Beable using Ancient Chinese Technique
 // @author       TheRealGeoDash
 // @match        *://*.beable.com/*
@@ -142,6 +142,24 @@
                 });
                 const prettied = sortedItems.map(e => ("" + e.category + "\n- " + ((e.values && e.values.length)? e.values.join("\n- ") : "[No Items in Category]")));
                 console.log("%c"+q.stimulus.replace(/&nbsp;/gmi, "")+"\n\n%c"+prettied.join("\n\n"), `font-size: 10px; color: #888888;`, `color: #44ff44; font-weight: bolder;`);
+            } else if (q.type === "imageclozeassociationV2") {
+                const mu = new MutationObserver(function() {
+                    document.awaitSelector(`.jss201.readaloud-block`).then(async () => {
+                        const dropZones = Array.from(document.querySelectorAll(`.jss201.readaloud-block .lrn_imagecloze_response .lrn_dragdrop.lrn_dropzone`));
+                        const dropElements = Array.from(document.querySelectorAll(`.jss201.readaloud-block .lrn_btn_drag.lrn_draggable`));
+                        
+                        for (const element of dropElements) {
+                            const elementIndex = dropElements.indexOf(element);
+                            element.click();
+                            dropZones[elementIndex].click();
+                            mu.disconnect();
+                        };
+                    });
+                });
+                mu.observe(document.querySelector(`body`), {
+                    subtree: true,
+                    childList: true,
+                });
             } else {
                 console.log("Unhandled Question:", q);
             }
